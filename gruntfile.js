4aa061c4dd5e83857903793409293e6f90defae7
Bryan Tong <contact@nullivex.com>
2013-12-15 20:20:35 -0700

    added grunt for dev help

---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt){
+
+  //config
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: true,
+        reporter: require('jshint-stylish')
+      },
+      server: ['**/*.js']
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/*.test.js']
+      }
+    },
+    watch: {
+      dev: {
+        files: ['**/*.js'],
+        tasks: ['test']
+      }
+    },
+    projectUpdate: {
+      update: {}
+    }
+  })
+
+  //load tasks
+  grunt.loadNpmTasks('grunt-contrib-jshint')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-mocha-test')
+  grunt.loadNpmTasks('grunt-project-update')
+
+  //macros
+  grunt.registerTask('update',['projectUpdate'])
+  grunt.registerTask('test',['jshint','mochaTest'])
+  grunt.registerTask('dev',['watch'])
+
+}
